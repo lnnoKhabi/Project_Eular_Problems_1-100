3f18aba8c05ba1b9f18a7dd61538f30ba873dcb7
inno_khabi <khabi98@gmail.com>
2020-11-06 00:10:09 +0200

    p29 and p30

--- ProjectEularProblems/Program.cs
+++ ProjectEularProblems/Program.cs
@@ -18,7 +18,7 @@ namespace ProjectEularProblems
         //Project Eular
         static void Main ( string[] args )
         {
-            DistinctPowers();
+            DigitFifthPowers();
             Console.ReadLine ();
         }
 
@@ -983,17 +983,31 @@ namespace ProjectEularProblems
         //PROBLEM 30
         /*Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
 
-        1634 = 14 + 64 + 34 + 44
-        8208 = 84 + 24 + 04 + 84
-        9474 = 94 + 44 + 74 + 44
-        As 1 = 14 is not a sum it is not included.
+        1634 = 1^4 + 6^4 + 3^4 + 4^4
+        8208 = 8^4 + 2^4 + 0^4 + 8^4
+        9474 = 9^4 + 4^4 + 7^4 + 4^4
+
+        As 1 = 1^4 is not a sum it is not included.
 
         The sum of these numbers is 1634 + 8208 + 9474 = 19316.
 
         Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.*/
         public static void DigitFifthPowers()
 		{
-
+            string n = "";
+            int res = 0;
+            double sum = 0;
+            for ( int i = 2; i <= 1000000; i++ )
+			{
+				n = i.ToString();
+                sum = 0;
+				for ( int j = 0; j < n.Length; j++ )
+				{
+                    sum += Math.Pow(int.Parse(n[ j ].ToString()), 5);
+				}
+                res = (int)sum == i ? res + i : res;
+			}
+			Console.WriteLine(res);
 		}
 
     }
