711f56a7ae3b21d5387f836761f301cbd34e546e
Inno Khabi <khabi98@gmail.com>
2021-02-04 23:39:40 +0200

    update

--- ProjectEularProblems/Program.cs
+++ ProjectEularProblems/Program.cs
@@ -1775,9 +1775,13 @@ namespace ProjectEularProblems
 			Console.WriteLine($"sum = {sum}");
 		}
 
-		//PROBLEM 37
-		/**/
-		public static void TrancactiblePrimes()
+        //PROBLEM 37
+        /*The number 3797 has an interesting property. Being prime itself, it is possible to continuously remove digits from left to right, and remain prime at each stage: 3797, 797, 97, and 7. Similarly we can work from right to left: 3797, 379, 37, and 3.
+
+        Find the sum of the only eleven primes that are both truncatable from left to right and right to left.
+
+        NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.*/
+        public static void TrancactiblePrimes()
 		{
             int max = 1000000;
             int[] primes = new int[ max ];
