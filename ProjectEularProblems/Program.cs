2d4c7a8f539690b3b264751b8374ada58d19d432
Inno Khabi <khabi98@gmail.com>
2021-05-31 23:08:13 +0200

    p59 XOR Decryption (solved)

--- ProjectEularProblems/Program.cs
+++ ProjectEularProblems/Program.cs
@@ -24,7 +24,7 @@ namespace ProjectEularProblems
 
 			Stopwatch sp = new Stopwatch();
 			sp.Start();
-			p058_SpiralPrimes();
+			p059_XORDecryption();
 			sp.Stop();
 			Console.WriteLine("\nruntime: " + sp.ElapsedMilliseconds / 1000.0 + "s");
 			Console.ReadLine();
@@ -3475,6 +3475,92 @@ namespace ProjectEularProblems
 			
 		}
 
+		/// <summary>
+		/// PROBLEM 59.
+		/// Each character on a computer is assigned a unique code and the preferred standard is ASCII (American Standard Code for Information Interchange). For example, uppercase A = 65, asterisk (*) = 42, and lowercase k = 107.
+
+		/// A modern encryption method is to take a text file, convert the bytes to ASCII, then XOR each byte with a given value, taken from a secret key.The advantage with the XOR function is that using the same encryption key on the cipher text, restores the plain text; for example, 65 XOR 42 = 107, then 107 XOR 42 = 65.
+
+		/// For unbreakable encryption, the key is the same length as the plain text message, and the key is made up of random bytes.The user would keep the encrypted message and the encryption key in different locations, and without both "halves", it is impossible to decrypt the message.
+
+		/// Unfortunately, this method is impractical for most users, so the modified method is to use a password as a key. If the password is shorter than the message, which is likely, the key is repeated cyclically throughout the message.The balance for this method is using a sufficiently long password key for security, but short enough to be memorable.
+
+		/// Your task has been made easy, as the encryption key consists of three lower case characters.Using p059_cipher.txt( right click and 'Save Link/Target As...'), a file containing the encrypted ASCII codes, and the knowledge that the plain text must contain common English words, decrypt the message and find the sum of the ASCII values in the original text.
+		/// </summary>
+		public static void p059_XORDecryption()
+		{
+			using(StreamReader sr = new StreamReader("p059_cipher.txt") )
+			{
+				int[] cipher = Array.ConvertAll( sr.ReadToEnd().Split(','), a => int.Parse(a));//text to be decrypted
+				string alphabet = "abcdefghijklmnopqrstuvwxyz";
+				List<string> keys = new List<string>();//for storing key permutations
+				string found = string.Empty;
+
+				//generate combinations in 3s then permute those combinations
+				for ( int i = 0; i < alphabet.Length-2; i++ )
+				{
+					for ( int j = i + 1; j < alphabet.Length - 1; j++ )
+					{
+						for ( int k = j + 1; k < alphabet.Length; k++ )
+						{
+							Permutation($"{alphabet[i]}{alphabet[ j ]}{alphabet[ k ]}", 0, 2, keys);//permutation of the combination
+							foreach ( string key in keys )
+							{
+								string sb = "";
+								int count = 0;//for repeating key cyclically
+								foreach ( int letter in cipher )
+								{
+									sb += char.ConvertFromUtf32(letter ^ key[ count ]);//xor cipher text with possible key
+									count++;
+									count = count == 3 ? 0 : count;
+								}
+
+								if ( sb.Contains(" the ") )
+								{
+									found = sb;
+									Console.WriteLine(sb);
+									Console.ForegroundColor = ConsoleColor.Green;
+									Console.WriteLine($"key = {key}");
+									Console.ForegroundColor = ConsoleColor.White;
+									goto end;
+								}
+
+							}
+							keys.Clear();
+						}
+					}
+				}
+				end:
+				Console.WriteLine(found.ToCharArray().Sum(a => a));
+			}
+		}
+		private static void Permutation( string str, int left, int right, List<string> res)
+		{
+			if ( left == right )
+			{
+				res.Add(str);
+			}
+			else
+			{
+				for ( int i = left; i <= right; i++ )
+				{
+					//swap(str[ left ], str[ i ]);
+					char[] lft = str.ToCharArray();
+					char c = str[ i ];
+					lft[ i ] = lft[ left ];
+					lft[ left ] = c;
+					str = new string(lft);
+
+					Permutation(str, left + 1, right, res);
+					//swap(str[ left ], str[ i ]); //swap back for backtracking
+					lft = str.ToCharArray();
+					c = str[ i ];
+					lft[ i ] = lft[ left ];
+					lft[ left ] = c;
+					str = new string(lft);
+				}
+			}
+		}
 	}
 }
 
